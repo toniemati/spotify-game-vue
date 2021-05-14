5a1846e1c2ba4db2083b527ce7df295718594305
toniemati <matikas2000@gmail.com>
2021-05-14 14:15:48 +0200

    working on game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="game">
-    <h1>game here</h1>
-    <button>start</button>
+    <p class="startBtn" @click="start">🍔</p>
+    <audio ref="audioEl"></audio>
+    <p v-for="(t, idx) in fourTracks" :key="idx">{{ t }}</p>
   </div>
 </template>
 
@@ -9,9 +10,31 @@
 export default {
   name: "Game",
   props: ['tracks'],
+  data() {
+    return {
+      fourTracks: [],
+    }
+  },
+  methods: {
+    start: function() {
+      this.fourTracks = [];
+
+      for (let i = 0; i < 4; i++) {
+        let idx = Math.floor(Math.random() * (this.tracks.length - 0 + 1)) + 0;
+        if (this.tracks[idx]) {
+          this.fourTracks.push(this.tracks[idx].name)
+        } else {
+          return;
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.startBtn {
+  cursor: pointer;
+  font-size: 2rem;
+}
 </style>
